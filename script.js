cc83d82811195cdccad454d80b3a83717b127c4e
Jim Margolis <57914060+JimMargolis@users.noreply.github.com>
2020-09-17 12:17:18 -0600

    Update script.js
    removes codes that changed "new post" to "new feature request"

--- script.js
+++ script.js
@@ -129,7 +129,4 @@ $(document).ready(function() {
     var isExpanded = this.getAttribute("aria-expanded") === "true";
     this.setAttribute("aria-expanded", !isExpanded);
   });
-
-   // change new post button to new feature request
-  $("a:contains('New post')").text("New Feature Request");
 });
